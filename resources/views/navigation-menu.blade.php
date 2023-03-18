JacobDeepu/kids-fest

Permission Management CRUD

resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
+                        {{ __('Permission') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -139,6 +142,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
+                {{ __('Permission') }}
             </x-jet-responsive-nav-link>
         </div>
 

KalyanDath/SimHealth

resources/views/navigation-menu.blade.php
@@ -21,6 +21,9 @@
                     <x-nav-link href="{{ route('doctor.index') }}" :active="request()->routeIs('doctor.index')">
                         {{ __('Doctor') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
+                        {{ __('Permission') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -151,6 +154,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('doctor.index') }}" :active="request()->routeIs('doctor')">
                 {{ __('Doctor') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
+                {{ __('Permission') }}
             </x-responsive-nav-link>
         </div>
 