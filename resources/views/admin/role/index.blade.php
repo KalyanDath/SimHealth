JacobDeepu/kids-fest

Role Management CRUD

resources/views/admin/role/index.blade.php
@@ -1,0 +1,88 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Role') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <div class="flex justify-between items-center">
+                        @can('role create')
+                        <x-link href="{{ route('role.create') }}" class="m-4">Create Role</x-link>
+                        @endcan
+                        <form class="m-4" method="GET" action="{{ route('role.index') }}">
+                            <div class="flex w-56">
+                                <div class="relative w-full">
+                                    <input name="search" class="inline-flex rounded-lg py-2 w-full z-20 text-sm text-gray-900 bg-gray-50  border border-gray-300 focus:ring-blue-500 focus:border-blue-500" type="search" value="{{ request()->input('search') }}" placeholder="Search..." required />
+                                    <x-jet-button class="absolute top-0 right-0">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg>
+                                        <span class="sr-only">Search</span>
+                                    </x-jet-button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('Name') }}
+                                </th>
+                                @canany(['role edit', 'role delete'])
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('Actions') }}
+                                </th>
+                                @endcanany
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($roles as $role)
+                            <tr class="bg-white border-b">
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{ $role->name }}
+                                </td>
+                                @canany(['role edit', 'role delete'])
+                                <td class="px-0 py-4 w-56">
+                                    <x-link href="{{ route('role.edit', $role) }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square w-4 h-4 mr-1">
+                                            <polyline points="9 11 12 14 22 4"></polyline>
+                                            <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+                                        </svg> Edit
+                                    </x-link>
+                                    <form method="POST" action="{{ route('role.destroy', $role) }}" class="inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                        <x-jet-danger-button type="submit" onclick="return confirm('Are you sure?')">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 w-4 h-4 mr-1">
+                                                <polyline points="3 6 5 6 21 6"></polyline>
+                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                                <line x1="10" y1="11" x2="10" y2="17"></line>
+                                                <line x1="14" y1="11" x2="14" y2="17"></line>
+                                            </svg> Delete
+                                        </x-jet-danger-button>
+                                    </form>
+                                </td>
+                                @endcanany
+                            </tr>
+                            @empty
+                            <tr class="bg-white border-b">
+                                <td colspan="2" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{ __('No roles Found') }}
+                                </td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                    <div class="px-6 py-4">
+                        {{ $roles->appends(request()->query())->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

resources/views/navigation-menu.blade.php
@@ -16,7 +16,10 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                     <x-jet-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
-                        {{ __('Permission') }}
+                        {{ __('Permissions') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('role.index') }}" :active="request()->routeIs('role.*')">
+                        {{ __('Roles') }}
                     </x-jet-nav-link>
                 </div>
             </div>
@@ -144,7 +147,10 @@
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
             <x-jet-responsive-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
-                {{ __('Permission') }}
+                {{ __('Permissions') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('role.index') }}" :active="request()->routeIs('role.*')">
+                {{ __('Roles') }}
             </x-jet-responsive-nav-link>
         </div>
 

KalyanDath/SimHealth

resources/views/admin/role/index.blade.php
@@ -1,0 +1,88 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Role') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <div class="flex justify-between items-center">
+                        @can('role create')
+                        <x-link href="{{ route('role.create') }}" class="m-4">Create Role</x-link>
+                        @endcan
+                        <form class="m-4" method="GET" action="{{ route('role.index') }}">
+                            <div class="flex w-56">
+                                <div class="relative w-full">
+                                    <input name="search" class="inline-flex rounded-lg py-2 w-full z-20 text-sm text-gray-900 bg-gray-50  border border-gray-300 focus:ring-blue-500 focus:border-blue-500" type="search" value="{{ request()->input('search') }}" placeholder="Search..." required />
+                                    <x-button class="absolute top-0 right-0">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg>
+                                        <span class="sr-only">Search</span>
+                                    </x-button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('Name') }}
+                                </th>
+                                @canany(['role edit', 'role delete'])
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('Actions') }}
+                                </th>
+                                @endcanany
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($roles as $role)
+                            <tr class="bg-white border-b">
+                                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{ $role->name }}
+                                </td>
+                                @canany(['role edit', 'role delete'])
+                                <td class="px-0 py-4 w-56">
+                                    <x-link href="{{ route('role.edit', $role) }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square w-4 h-4 mr-1">
+                                            <polyline points="9 11 12 14 22 4"></polyline>
+                                            <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
+                                        </svg> Edit
+                                    </x-link>
+                                    <form method="POST" action="{{ route('role.destroy', $role) }}" class="inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                        <x-danger-button type="submit" onclick="return confirm('Are you sure?')">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 w-4 h-4 mr-1">
+                                                <polyline points="3 6 5 6 21 6"></polyline>
+                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                                <line x1="10" y1="11" x2="10" y2="17"></line>
+                                                <line x1="14" y1="11" x2="14" y2="17"></line>
+                                            </svg> Delete
+                                        </x-danger-button>
+                                    </form>
+                                </td>
+                                @endcanany
+                            </tr>
+                            @empty
+                            <tr class="bg-white border-b">
+                                <td colspan="2" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{ __('No roles Found') }}
+                                </td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                    <div class="px-6 py-4">
+                        {{ $roles->appends(request()->query())->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

resources/views/navigation-menu.blade.php
@@ -24,6 +24,9 @@
                     <x-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
                         {{ __('Permission') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('role.index') }}" :active="request()->routeIs('role.*')">
+                        {{ __('Roles') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -157,6 +160,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('permission.index') }}" :active="request()->routeIs('permission.*')">
                 {{ __('Permission') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('role.index') }}" :active="request()->routeIs('role.*')">
+                {{ __('Roles') }}
             </x-responsive-nav-link>
         </div>
 